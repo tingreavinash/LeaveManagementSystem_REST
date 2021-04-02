53b2e8d29bfefb3f74e44be4588ebd1952ba8fb5
Avinash Tingre <tingre.avinash@gmail.com>
2021-04-02 13:51:25 +0530

    Replace swagger with Swagger 2

--- src/main/java/com/avinash/leavemanagementsystem/LeavemanagementsystemApplication.java
+++ src/main/java/com/avinash/leavemanagementsystem/LeavemanagementsystemApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 
 
 @SpringBootApplication
@@ -19,6 +16,7 @@ public class LeavemanagementsystemApplication {
 	
 	}
 	
+/*
 	@Bean
     public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
      return new OpenAPI()
@@ -29,6 +27,7 @@ public class LeavemanagementsystemApplication {
           .termsOfService("http://swagger.io/terms/")
           .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
+*/
 
 	
 	
